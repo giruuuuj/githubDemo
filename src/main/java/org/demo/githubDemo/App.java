e1f956532fc10262edda653368b1d5edac3fd881
ABHIJEET KALE <abhijeetkale097@gmail.com>
2025-12-28 02:49:32 +0530

    remove version

--- src/main/java/org/demo/githubDemo/App.java
+++ src/main/java/org/demo/githubDemo/App.java
@@ -10,6 +10,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "This is a demo project for GitHub." );
-        System.out.println( "Enjoy coding!" );
     }
 }
